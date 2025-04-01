33d20860a1b8ce5533e6775ceaaaba310a2512fe
DimitarSimonoski <129298872+DimitarSimonoski@users.noreply.github.com>
2025-04-01 14:35:49 +0200

    fix: fix UserModel and UserException

--- Choremonkeys/src/main/java/com/example/choremonkeys/models/exceptions/InvalidUserIdExcpetion.java
+++ Choremonkeys/src/main/java/com/example/choremonkeys/models/exceptions/InvalidUserIdExcpetion.java
@@ -1,4 +1,7 @@
 package com.example.choremonkeys.models.exceptions;
 
 public class InvalidUserIdExcpetion extends RuntimeException {
+    public InvalidUserIdExcpetion () {
+        super ("Invalid UserId");
+    }
 }
